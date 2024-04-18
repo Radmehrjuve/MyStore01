6028f73bd563e10974c95b8637d1f4b3926a256a
Radmehrjuve <Radmehrvosooghi@gmail.com>
2024-04-18 15:53:22 +0330

    Signup and Login Model Classes

---
+++ MyStore01.WebUI/Models/Users Info/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore01.WebUI.Models.Users_Info
+{
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("Email")]
+        public string Email { get; set; } 
+        [Required]
+        [UIHint("Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ MyStore01.WebUI/Models/Users Info/SignupModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore01.WebUI.Models.Users_Info
+{
+    public class SignupModel
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
